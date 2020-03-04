39294a2805c113be5eb7b9345d54269d8177f9ef
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2020-03-03 22:58:23 -0600

    Inicio de main con simple sintaxis con boton de verificar y boton de limpiar(funcionable)

---
+++ main.js
@@ -0,0 +1,17 @@
+var btnVerificar = document.querySelector("#btnVerificar")
+var btnBorrar = document.querySelector("#btnLimpiar")
+
+btnVerificar.addEventListener('click', () => {
+    
+})
+
+btnLimpiar.addEventListener('click', ()=>{
+    
+    valorX0.value = null
+    valorX1.value = null
+    valorX2.value = null
+    valorFX0.value = null
+    valorFX1.value = null
+    valorFX2.value = null
+    
+  })
